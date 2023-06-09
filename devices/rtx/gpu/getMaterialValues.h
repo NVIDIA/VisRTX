52dccf22ea11a8462b6c433596d6f747e7b4fb62
Jefferson Amstutz <jamstutz@nvidia.com>
2023-06-09 14:17:43 -0500

    fix calculating transformed values prior to using samplers

--- devices/rtx/gpu/getMaterialValues.h
+++ devices/rtx/gpu/getMaterialValues.h
@@ -230,7 +230,7 @@ RT_FUNCTION T evaluateSampler(
   vec4 retval{0.f};
   const auto &sampler = getSamplerData(fd, _s);
   const vec4 tc =
-      readAttributeValue(sampler.attribute, hit) * sampler.inTransform;
+      sampler.inTransform * readAttributeValue(sampler.attribute, hit);
   switch (sampler.type) {
   case SamplerType::TEXTURE1D: {
     retval = make_vec4(tex1D<::float4>(sampler.image1D.texobj, tc.x));
