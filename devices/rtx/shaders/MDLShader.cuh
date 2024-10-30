81ba4947bece0bf29e34b9dd3900c2b3968c1f37
Thomas Arcila <tarcila@nvidia.com>
2024-10-30 15:40:49 -0400

    Return some nicer white color instead of rendering black on missing shader
    Still to be improved with an actual builtin shader implementation that we
    can guarantee will always be valid.

--- devices/rtx/shaders/MDLShader.cuh
+++ devices/rtx/shaders/MDLShader.cuh
@@ -17,16 +17,19 @@ VISRTX_DEVICE vec4 shadeMDLSurface(const FrameGPUData &fd,
     const LightSample &ls)
 {
   // Call signature must match the actual implementation in MDLShader_ptx.cu
-  return optixDirectCall<vec4>(
-      // FIXME: MDL base implementation index, in the Sbt, need to be properly
-      // computed and setup time and runtime shared with the compute code.
-      static_cast<unsigned int>(MaterialType::MDL) + md.implementationId,
-      &fd,
-      &ss,
-      &md,
-      &ray,
-      &hit,
-      &ls);
+  if (md.implementationId == -1ul) {
+    return vec4(0.8f, 0.8f, 0.8f, 1.0f);
+  } else
+    return optixDirectCall<vec4>(
+        // FIXME: MDL base implementation index, in the Sbt, need to be properly
+        // computed and setup time and runtime shared with the compute code.
+        static_cast<unsigned int>(MaterialType::MDL) + md.implementationId,
+        &fd,
+        &ss,
+        &md,
+        &ray,
+        &hit,
+        &ls);
 }
 
 } // namespace visrtx
