a8def7f1cc2b1dfcd700fced17e8202eb0775e97
Jefferson Amstutz <jamstutz@nvidia.com>
2025-03-04 10:08:14 -0600

    remove obsolete NVDB volume parameter

--- tsd/src/tsd/authoring/importers/import_NVDB.cpp
+++ tsd/src/tsd/authoring/importers/import_NVDB.cpp
@@ -102,11 +102,6 @@ SpatialFieldRef import_NVDB(Context &ctx, const char *filepath)
     auto gridData = ctx.createArray(ANARI_UINT8, grid.size());
     std::memcpy(gridData->map(), grid.data(), grid.size());
     gridData->unmap();
-
-    // gridData is the parameter name used by visionaray. Keep this for now
-    // waiting for the switch to the more general data named parameter, as used
-    // by the structured reguar field type.
-    field->setParameterObject("gridData", *gridData);
     field->setParameterObject("data", *gridData);
 
     logStatus("[import_NVDB] ...done!");
