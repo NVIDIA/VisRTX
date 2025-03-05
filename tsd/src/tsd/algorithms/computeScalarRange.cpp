d073178b0a82647494e49a876f38cc7b34577fb8
Jefferson Amstutz <jamstutz@nvidia.com>
2025-03-04 21:13:09 -0600

    speed up CUDA enabled TSD builds by splitting computeScalarRange into TUs

--- tsd/src/tsd/algorithms/computeScalarRange.cpp
+++ tsd/src/tsd/algorithms/computeScalarRange.cpp
@@ -1,86 +1,13 @@
 // Copyright 2024-2025 NVIDIA Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef TSD_USE_CUDA
-#define TSD_USE_CUDA 1
-#endif
-
 #include "tsd/algorithms/computeScalarRange.hpp"
 #include "tsd/core/Context.hpp"
-// std
-#include <algorithm>
-#include <limits>
-#if TSD_USE_CUDA
-// thrust
-#include <cuda_runtime.h>
-#include <thrust/device_ptr.h>
-#include <thrust/extrema.h>
-#endif
+#include "tsd/core/Logging.hpp"
+
+#include "tsd/algorithms/detail/computeScalarRangeImpl.hpp"
 
 namespace tsd::algorithm {
-
-namespace detail {
-
-// NOTE(jda): This is a reduced version of anari::anariTypeInvoke() to lower
-//            Thrust/CUDA compile times
-template <typename R, template <int> class F, typename... Args>
-inline R scalarTypeInvoke(ANARIDataType type, Args &&...args)
-{
-  // clang-format off
-  switch (type) {
-  case ANARI_UFIXED8: return F<ANARI_UFIXED8>()(std::forward<Args>(args)...);
-  case ANARI_UFIXED16: return F<ANARI_UFIXED16>()(std::forward<Args>(args)...);
-  case ANARI_FIXED8: return F<ANARI_FIXED8>()(std::forward<Args>(args)...);
-  case ANARI_FIXED16: return F<ANARI_FIXED16>()(std::forward<Args>(args)...);
-  case ANARI_FLOAT32: return F<ANARI_FLOAT32>()(std::forward<Args>(args)...);
-  case ANARI_FLOAT64: return F<ANARI_FLOAT64>()(std::forward<Args>(args)...);
-  default:
-    return F<ANARI_UNKNOWN>()(std::forward<Args>(args)...);
-  }
-  // clang-format off
-}
-
-template <int ANARI_ENUM_T>
-struct ComputeScalarRange
-{
-  using properties_t = anari::ANARITypeProperties<ANARI_ENUM_T>;
-  using base_t = typename properties_t::base_type;
-
-  tsd::float2 operator()(const Array &a)
-  {
-    tsd::float4 min_out{0.f, 0.f, 0.f, 0.f};
-    tsd::float4 max_out{0.f, 0.f, 0.f, 0.f};
-
-    const auto *begin = a.dataAs<base_t>();
-    const auto *end = begin + a.size();
-#if TSD_USE_CUDA
-    if (a.kind() == Array::MemoryKind::CUDA) {
-      const auto minmax = thrust::minmax_element(
-          thrust::device_pointer_cast(begin), thrust::device_pointer_cast(end));
-      const base_t min_v = *minmax.first;
-      const base_t max_v = *minmax.second;
-      properties_t::toFloat4(&min_out.x, &min_v);
-      properties_t::toFloat4(&max_out.x, &max_v);
-    } else {
-#endif
-      const auto minmax = std::minmax_element(begin, end);
-      const auto min_v = *minmax.first;
-      const auto max_v = *minmax.second;
-      properties_t::toFloat4(&min_out.x, &min_v);
-      properties_t::toFloat4(&max_out.x, &max_v);
-#if TSD_USE_CUDA
-    }
-#endif
-
-    return {min_out.x, max_out.x};
-  }
-};
-
-} // namespace detail
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
 
 tsd::float2 computeScalarRange(const Array &a)
 {
@@ -103,8 +30,32 @@ tsd::float2 computeScalarRange(const Array &a)
       retval.y = std::max(retval.y, subRange.y);
     });
   } else if (elementsAreScalars) {
-    retval = detail::scalarTypeInvoke<tsd::float2, detail::ComputeScalarRange>(
-        type, a);
+    switch (type) {
+    case ANARI_UFIXED8:
+      retval = detail::computeScalarRange_ufixed8(a);
+      break;
+    case ANARI_UFIXED16:
+      retval = detail::computeScalarRange_ufixed16(a);
+      break;
+    case ANARI_FIXED8:
+      retval = detail::computeScalarRange_fixed8(a);
+      break;
+    case ANARI_FIXED16:
+      retval = detail::computeScalarRange_fixed16(a);
+      break;
+    case ANARI_FLOAT32:
+      retval = detail::computeScalarRange_float32(a);
+      break;
+    case ANARI_FLOAT64:
+      retval = detail::computeScalarRange_float64(a);
+      break;
+    default:
+      logWarning(
+          "computeScalarRange() called on an "
+          "array with incompatible element type '%s'",
+          anari::toString(type));
+      break;
+    }
   }
 
   return retval;
