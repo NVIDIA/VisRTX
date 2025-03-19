e44b1800f953f11d376cb5aa479a7474faaa9525
Jefferson Amstutz <jamstutz@nvidia.com>
2025-03-19 12:58:19 -0500

    revert back to denoisef 'off' by default

--- devices/rtx/renderer/Renderer.cpp
+++ devices/rtx/renderer/Renderer.cpp
@@ -160,7 +160,7 @@ void Renderer::commitParameters()
       getParam<float>("ambientRadiance", m_defaultAmbientRadiance);
   m_occlusionDistance = getParam<float>("ambientOcclusionDistance", 1e20f);
   m_checkerboard = getParam<bool>("checkerboarding", false);
-  m_denoise = getParam<bool>("denoise", true);
+  m_denoise = getParam<bool>("denoise", false);
   m_sampleLimit = getParam<int>("sampleLimit", 128);
   m_cullTriangleBF = getParam<bool>("cullTriangleBackfaces", false);
   m_volumeSamplingRate =
