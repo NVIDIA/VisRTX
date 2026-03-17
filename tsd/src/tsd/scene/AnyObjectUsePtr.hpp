714e7fcf1b00ed3599b0a1e53f1ded25a99b86a7
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2026-03-17 09:26:42 -0400

    Move AnyObjectUsePtr to inline
    In preparation for templetization of the class.

--- tsd/src/tsd/scene/AnyObjectUsePtr.hpp
+++ tsd/src/tsd/scene/AnyObjectUsePtr.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "tsd/scene/Object.hpp"
+#include "tsd/scene/Scene.hpp"
 
 namespace tsd::scene {
 
@@ -55,6 +56,120 @@ bool operator==(const AnyObjectUsePtr &a, const AnyObjectUsePtr &b);
 bool operator!=(const AnyObjectUsePtr &a, const AnyObjectUsePtr &b);
 
 // Inlined definitions ////////////////////////////////////////////////////////
+
+inline AnyObjectUsePtr::AnyObjectUsePtr(Object &o)
+{
+  reset();
+}
+
+inline AnyObjectUsePtr::AnyObjectUsePtr(const AnyObjectUsePtr &o)
+{
+  m_object = o.m_object;
+  m_scene = o.m_scene;
+  if (auto *obj = get(); obj != nullptr)
+    obj->incUseCount(Object::UseKind::APP);
+}
+
+inline AnyObjectUsePtr::AnyObjectUsePtr(AnyObjectUsePtr &&o)
+{
+  m_object = std::move(o.m_object);
+  m_scene = o.m_scene;
+  o.m_scene = nullptr;
+  o.m_object.reset();
+}
+
+inline AnyObjectUsePtr::~AnyObjectUsePtr()
+{
+  reset();
+}
+
+inline AnyObjectUsePtr &AnyObjectUsePtr::operator=(const AnyObjectUsePtr &o)
+{
+  if (this != &o) {
+    reset();
+    m_object = o.m_object;
+    m_scene = o.m_scene;
+    if (auto *obj = get(); obj != nullptr)
+      obj->incUseCount(Object::UseKind::APP);
+  }
+  return *this;
+}
+
+inline AnyObjectUsePtr &AnyObjectUsePtr::operator=(AnyObjectUsePtr &&o)
+{
+  m_object = std::move(o.m_object);
+  m_scene = o.m_scene;
+  o.m_scene = nullptr;
+  o.m_object.reset();
+  return *this;
+}
+
+inline AnyObjectUsePtr &AnyObjectUsePtr::operator=(Object &o)
+{
+  reset();
+  if (o.scene() != nullptr) {
+    m_scene = o.scene();
+    m_object = Any(o.type(), o.index());
+    o.incUseCount(Object::UseKind::APP);
+  }
+  return *this;
+}
+
+inline void AnyObjectUsePtr::reset()
+{
+  if (auto *obj = get(); obj != nullptr)
+    obj->decUseCount(Object::UseKind::APP);
+
+  m_scene = nullptr;
+}
+
+inline const Object *AnyObjectUsePtr::get() const
+{
+  return m_scene && m_object ? m_scene->getObject(m_object) : nullptr;
+}
+
+inline const Object *AnyObjectUsePtr::operator->() const
+{
+  return get();
+}
+
+inline const Object &AnyObjectUsePtr::operator*() const
+{
+  return *get();
+}
+
+inline Object *AnyObjectUsePtr::get()
+{
+  return m_scene && m_object ? m_scene->getObject(m_object) : nullptr;
+}
+
+inline Object *AnyObjectUsePtr::operator->()
+{
+  return get();
+}
+
+inline Object &AnyObjectUsePtr::operator*()
+{
+  return *get();
+}
+
+inline AnyObjectUsePtr::operator bool() const
+{
+  return get() != nullptr;
+}
+
+inline bool operator==(const AnyObjectUsePtr &a, const AnyObjectUsePtr &b)
+{
+  auto *a1 = a.get();
+  auto *b1 = b.get();
+  return (a1 && b1) && (a1->type() == b1->type())
+      && (a1->index() == b1->index());
+}
+
+inline bool operator!=(const AnyObjectUsePtr &a, const AnyObjectUsePtr &b)
+{
+  return !(a == b);
+}
 
 template <typename T>
 inline const T *AnyObjectUsePtr::getAs() const
