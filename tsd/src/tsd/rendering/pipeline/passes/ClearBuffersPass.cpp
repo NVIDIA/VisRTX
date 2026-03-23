48915f722747d7229df28c49ffc1f450274839d0
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2026-03-23 14:47:13 -0400

    Update ClearBuffersPass so it uses the right cuda stream

--- tsd/src/tsd/rendering/pipeline/passes/ClearBuffersPass.cpp
+++ tsd/src/tsd/rendering/pipeline/passes/ClearBuffersPass.cpp
@@ -3,10 +3,14 @@
 
 #include "ClearBuffersPass.h"
 // std
-#include <algorithm>
 #include <limits>
 
-#include "detail/parallel_for.h"
+#ifdef ENABLE_CUDA
+#include <thrust/execution_policy.h>
+#include <thrust/fill.h>
+#else
+#include <algorithm>
+#endif
 
 namespace tsd::rendering {
 
@@ -21,22 +25,25 @@ void ClearBuffersPass::setClearColor(const tsd::math::float4 &color)
 void ClearBuffersPass::render(ImageBuffers &b, int /*stageId*/)
 {
   const auto size = getDimensions();
-  const size_t totalSize = size_t(size.x) * size_t(size.y);
+  const uint32_t totalPixels = uint32_t(size.x) * uint32_t(size.y);
   const auto c = helium::cvt_color_to_uint32(m_clearColor);
-#if ENABLE_CUDA
-  thrust::fill(b.color, b.color + totalSize, c);
-  thrust::fill(b.hdrColor, b.hdrColor + totalSize * 4, 0.f);
-  thrust::fill(b.depth, b.depth + totalSize, tsd::core::math::inf);
-  thrust::fill(b.objectId, b.objectId + totalSize, ~0u);
-  thrust::fill(b.primitiveId, b.primitiveId + totalSize, ~0u);
-  thrust::fill(b.instanceId, b.instanceId + totalSize, ~0u);
+  const float inf = std::numeric_limits<float>::infinity();
+
+#ifdef ENABLE_CUDA
+  auto policy = thrust::cuda::par.on(b.stream);
+  thrust::fill(policy, b.color, b.color + totalPixels, c);
+  thrust::fill(policy, b.hdrColor, b.hdrColor + totalPixels * 4, 0.f);
+  thrust::fill(policy, b.depth, b.depth + totalPixels, inf);
+  thrust::fill(policy, b.objectId, b.objectId + totalPixels, ~0u);
+  thrust::fill(policy, b.primitiveId, b.primitiveId + totalPixels, ~0u);
+  thrust::fill(policy, b.instanceId, b.instanceId + totalPixels, ~0u);
 #else
-  std::fill(b.color, b.color + totalSize, c);
-  std::fill(b.hdrColor, b.hdrColor + totalSize * 4, 0.f);
-  std::fill(b.depth, b.depth + totalSize, tsd::core::math::inf);
-  std::fill(b.objectId, b.objectId + totalSize, ~0u);
-  std::fill(b.primitiveId, b.primitiveId + totalSize, ~0u);
-  std::fill(b.instanceId, b.instanceId + totalSize, ~0u);
+  std::fill(b.color, b.color + totalPixels, c);
+  std::fill(b.hdrColor, b.hdrColor + totalPixels * 4, 0.f);
+  std::fill(b.depth, b.depth + totalPixels, inf);
+  std::fill(b.objectId, b.objectId + totalPixels, ~0u);
+  std::fill(b.primitiveId, b.primitiveId + totalPixels, ~0u);
+  std::fill(b.instanceId, b.instanceId + totalPixels, ~0u);
 #endif
 }
 
