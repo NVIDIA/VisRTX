49f9158c10c55e8c49df52bbfec35ddde7eaa303
Jefferson Amstutz <jamstutz@nvidia.com>
2026-03-25 13:46:09 -0500

    fix initialization order bug

--- tsd/src/tsd/animation/TransformBinding.cpp
+++ tsd/src/tsd/animation/TransformBinding.cpp
@@ -12,7 +12,7 @@ namespace tsd::animation {
 TransformBinding::TransformBinding(scene::Scene *scene) : Binding(scene) {}
 
 TransformBinding::TransformBinding(scene::LayerNodeRef target)
-    : Binding((*m_target)->layer()->scene()), m_target(target)
+    : Binding((*target)->layer()->scene()), m_target(target)
 {}
 
 TransformBinding::TransformBinding(scene::LayerNodeRef target,
